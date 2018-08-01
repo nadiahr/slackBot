0acf07df3d782fd09a535bab01f5b38974587e5b
nadia <nadia@localhost.localdomain>
2018-08-01 14:16:54 +0200

    commit3

--- src/main/java/com/example/demo/SlackBotApplication.java
+++ src/main/java/com/example/demo/SlackBotApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.example.demo"})
-public class SlackBotApplication {
+
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.example.slackbot"})
+public class SlackBotApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+System.out.println("goooooooooo");
 		SpringApplication.run(SlackBotApplication.class, args);
+		
 	}
 }
